67a5d6651431ed36586932cf06c726cf4aae36f1
nathan m weller <nkas17@gmail.com>
2019-04-07 00:17:45 -0400

    setup cors

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 const mlabKey = process.env.MLAB_KEY;
-const db = process.env.NODE_ENV === 'production' ? 'recipes' : 'recipes-test';
+const isProdEnv = process.env.NODE_ENV === 'production';
+const db = isProdEnv ? 'recipes' : 'recipes-test';
 const url = `https://api.mlab.com/api/1/databases/${db}/collections/recipes?apiKey=${mlabKey}`;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", isProdEnv ? 'https://www.recipes.nathanmweller.com' : 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
