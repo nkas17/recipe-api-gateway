d584f3714a843d5516d9c8e01aaa4f3aba45ab39
nathan m weller <nkas17@gmail.com>
2019-04-07 01:21:48 -0400

    added auth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request-promise');
+const auth = require('basic-auth')
 
 require('dotenv').config();
 
@@ -9,6 +10,17 @@ const mlabKey = process.env.MLAB_KEY;
 const isProdEnv = process.env.NODE_ENV === 'production';
 const db = isProdEnv ? 'recipes' : 'recipes-test';
 const url = `https://api.mlab.com/api/1/databases/${db}/collections/recipes?apiKey=${mlabKey}`;
+ 
+app.use((req, res, next) => {
+	const credentials = auth(req)
+  if (!credentials || !(credentials.name === process.env.USER && credentials.pass === process.env.USER_PW)) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="http://localhost:3000"')
+    res.end('Access denied')
+  } else {
+    next();
+  }
+})
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", isProdEnv ? 'https://www.recipes.nathanmweller.com' : 'http://localhost:8080');
