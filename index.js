a96c3890fdb77e0de9f44b7e30bda40921753bbf
nathan m weller <nkas17@gmail.com>
2019-04-07 01:58:11 -0400

    remove auth add login route

--- index.js
+++ index.js
@@ -11,25 +11,29 @@ const isProdEnv = process.env.NODE_ENV === 'production';
 const db = isProdEnv ? 'recipes' : 'recipes-test';
 const url = `https://api.mlab.com/api/1/databases/${db}/collections/recipes?apiKey=${mlabKey}`;
  
-app.use((req, res, next) => {
-	const credentials = auth(req)
-  if (!credentials || !(credentials.name === process.env.USER && credentials.pass === process.env.USER_PW)) {
-    res.statusCode = 401
-    res.setHeader('WWW-Authenticate', 'Basic realm="http://localhost:3000"')
-    res.end('Access denied')
-  } else {
-    next();
-  }
-})
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", isProdEnv ? 'https://www.recipes.nathanmweller.com' : 'http://localhost:8080');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/login', (req, res, next) => {
+	const credentials = auth(req)
+  if (!credentials || !(credentials.name === process.env.USER && credentials.pass === process.env.USER_PW)) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="allow access"')
+    res.end('Access denied')
+  } else {
+    res.send("granted");
+  }
+})
 
 app.get('/recipe',(req, res) => {
 	request.get({
