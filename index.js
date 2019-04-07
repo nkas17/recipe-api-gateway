3a085b5b249b0b1668041f95c9d77c6a30180296
nathan m weller <nkas17@gmail.com>
2019-04-06 23:27:01 -0400

    new get recipes route

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const request = require('request-promise');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const mlabKey = process.env.MLAB_KEY;
+const db = process.env.NODE_ENV === 'production' ? 'recipes' : 'recipes-test';
+const url = `https://api.mlab.com/api/1/databases/${db}/collections/recipes?apiKey=${mlabKey}`;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/recipe',(req, res) => {
+	request.get({
+		url,
+	}).then((data)=>{
+		res.send(data);
+	})
+	
+})
+
+app.listen(port, () => console.log(`recipe-api-gateway listening on port ${port}!`))
