933d3d53c4e30fd5c2d7cc3dfc5d006f12ca2c73
nathan m weller <nkas17@gmail.com>
2019-04-20 09:39:49 -0400

    remove www in access header

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", isProdEnv ? 'https://www.recipes.nathanmweller.com' : 'http://localhost:8080');
+  res.header("Access-Control-Allow-Origin", isProdEnv ? 'https://recipes.nathanmweller.com' : 'http://localhost:8080');
   res.header('Vary', 'Origin');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
