8fad247bd746194eace20076d6fe27616c7d4cc0
nathan m weller <nkas17@gmail.com>
2020-10-06 18:10:59 -0400

    update db url to atlas

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.59wyw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 module.exports = {
 	isProdEnv: process.env.NODE_ENV === 'production',
 	port: process.env.PORT || 3000,
 	user: process.env.USER,
 	password: process.env.USER_PW,
 	jwtKey: process.env.JWT_KEY,
-	mongoUrl: `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds1${process.env.DB_PORT}.mlab.com:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+	mongoUrl,
 };
