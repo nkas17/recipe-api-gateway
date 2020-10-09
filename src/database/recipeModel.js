7ffce640049d5c6ba97e568625cc914545092895
nathan m weller <nkas17@gmail.com>
2020-10-09 18:05:36 -0400

    use objects for schema def

--- src/database/recipeModel.js
+++ src/database/recipeModel.js
@@ -5,12 +5,12 @@ const { Schema } = mongoose;
 const Recipe = mongoose.model(
 	'recipe',
 	new Schema({
-		title: String,
-		description: String,
-		category: String,
-		id: String,
-		ingredients: String,
-		directions: String,
+		title: { type: String },
+		description: { type: String },
+		category: { type: String },
+		id: { type: String },
+		ingredients: { type: String },
+		directions: { type: String },
 	})
 );
 
